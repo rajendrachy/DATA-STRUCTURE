825306f2b672d3a1a25d8622682d3e5351374002
rajendrachy <chyrajendra32@gmail.com>
2025-07-22 09:30:38 +0530

    solve 5 problems of array

---
+++ DAY7/SumOFArray.java
@@ -0,0 +1,32 @@
+// Sum of Array
+// Difficulty: BasicAccuracy: 74.98%Submissions: 131K+Points: 1Average Time: 20m
+// You are given an integer array arr[]. The task is to find the sum of it.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4]
+// Output: 10
+// Explanation: 1 + 2 + 3 + 4 = 10.
+// Input: arr[] = [1, 3, 3]
+// Output: 7
+// Explanation: 1 + 3 + 3 = 7.
+
+
+
+
+
+// class Solution {
+//     int arraySum(int arr[]) {
+//         // code here
+//         int sum = 0;
+        
+//         for(int i=0; i<arr.length; i++) {
+//             sum += arr[i];
+//         }
+        
+//         return sum;
+//     }
+// }
+
+
+
